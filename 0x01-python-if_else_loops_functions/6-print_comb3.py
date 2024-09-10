1d143cf7e8ef3018217e7f48bbd51f4f7883fc4d
ITSNE01 <rabie.haddar1@gmail.com>
2024-09-10 13:43:45 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,7 +6,7 @@ for i in range(0, 10):
     for j in range(i + 1, 10):
         # Check if it's the last combo (89) to print
         if i == 8 and j == 9:
-            print(f"{i}{j}")
+            print("89")
         else:
             # Print the combo followed by ", " w/out newline
-            print(f"{i}{j}, ", end="")
+            print('{}{}, '.format(i, j), end="")
