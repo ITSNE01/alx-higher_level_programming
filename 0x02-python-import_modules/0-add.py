46298568b12c09717967e36c8de791b00380de29
ITSNE01 <rabie.haddar1@gmail.com>
2024-09-12 13:10:56 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__";
     """Print the sum of a and b"""
 
-    # Importing the 'add' function from the module 'add_0'
+    # Import 'add' function from module 'add_0'
     from add_0 import add
     a = 1
     b = 2
-    # Print the sum of x and y using the 'add' function
+    # Print sum of a and b using 'add' function
     print("{} + {} = {}".format(a, b, add(a, b)))
