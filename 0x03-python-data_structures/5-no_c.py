ff78668e19ab7afc3928e80e8b4be9df8e50fdf7
ITSNE01 <rabie.haddar1@gmail.com>
2024-09-15 14:15:19 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Create a new str by filtering out 'c' & 'C'
-    new_string = ''.join(char for char in my_string if char != 'c' and char != 'C')
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
     return new_string
