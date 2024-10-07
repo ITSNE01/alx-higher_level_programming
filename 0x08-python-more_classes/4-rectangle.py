858fb9a9c75ed238609904cb2c736a31d5faf0ea
ITSNE01 <rabie.haddar1@gmail.com>
2024-10-07 13:20:35 -0700

    5-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
 This is the "Rectangle" module.
+
 This module provides a simple Rectangle class.
 """
 
 
 class Rectangle:
     """A Rectangle class with attributes width and height, and
-    methods area, perimeter, print, str, and repr"""
+    methods area, perimeter, print, str, and repr
+    """
     
     def __init__(self, width=0, height=0):
         self.width = width
@@ -38,7 +40,6 @@ class Rectangle:
         self.__height = value
 
     def __str__(self):
-        """Prints the rectangle using the `#` character."""
         total = ""
         if self.__height == 0 or self.__width == 0:
             return total
@@ -49,15 +50,12 @@ class Rectangle:
         return total
 
     def __repr__(self):
-        """Returns a str repr of rectangle that can recreate an instance."""
         return f"Rectangle({self.__width}, {self.__height})"
 
     def area(self):
-        """Returns the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Returns perimeter of rectangle. If width/height is 0, returns 0."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
 This is the "Rectangle" module.
+
 This module provides a simple Rectangle class.
 """
 
 
 class Rectangle:
     """A Rectangle class with attributes width and height, and
-    methods area, perimeter, print, str, repr, and deletion message"""
-
+    methods area, perimeter, print, str, repr, and deletion message
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -38,25 +39,20 @@ class Rectangle:
         self.__height = value
 
     def __str__(self):
-        """Prints the rectangle using the `#` character."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Returns a str repr of rectangle that can recreate an instance."""
         return f"Rectangle({self.__width}, {self.__height})"
 
     def area(self):
-        """Returns the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Returns the perimeter of rectangle. If width/height is 0, returns 0."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __del__(self):
-        """Prints a message when the rectangle is deleted."""
         print("Bye rectangle...")
