289668112a16c419a46205efeb37a98165e15021
ITSNE01 <rabie.haddar1@gmail.com>
2024-10-08 11:31:39 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/python3
-def magic_string(magic=[]):
-    magic += ["BestSchool"]
-    return ", ".join(magic)
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)]
