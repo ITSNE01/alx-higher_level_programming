e594bc26a8125cd9e3ba084930bae10f44ccada7
ITSNE01 <rabie.haddar1@gmail.com>
2024-10-14 12:54:20 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module for inherits_from method
+'''
+
+
+def inherits_from(obj, a_class):
+    """ Determines if obj is a true subclass of a_class """
+    if (type(obj) != a_class):
+        return isinstance(obj, a_class)
+    return False
