dbfab491b2f78a6f21a8d4b970b061e290dd9224
ITSNE01 <rabie.haddar1@gmail.com>
2024-10-15 15:29:43 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student Creation Method"""
+
+
+class Student:
+    """Student representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of student"""
+        if attrs is None:
+            return self.__dict__
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return new_dictionary
