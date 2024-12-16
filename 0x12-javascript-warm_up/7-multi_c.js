86ba3e5f3d5d390a85c242000ff53e56777f3c8f
ITSNE01 <rabie.haddar1@gmail.com>
2024-12-16 11:29:52 -0800

    fix 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 
 let num = Number(process.argv[2]);
 if (isNaN(num)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	while (num > 0) {
-		console.log('C is fun');
-		--num;
-	}
+  while (num > 0) {
+    console.log('C is fun');
+    --num;
+  }
 }
