1c23ab6b24f8de6c9449b83dd49fbc43feb9fc0d
ITSNE01 <rabie.haddar1@gmail.com>
2025-02-11 03:28:22 -0800

    4-starwars_count.js FIX

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,35 +2,13 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-
-if (!apiUrl) {
-  console.error('Usage: node 4-starwars_count.js <API_URL>');
-  process.exit(1);
-}
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error: Unable to fetch data (Status Code: ${response.statusCode})`);
-    return;
-  }
-
-  try {
-    const data = JSON.parse(body);
-    const films = data.results;
-    const wedgeId = '18';
-    
-    const count = films.filter(film => 
-      film.characters.some(character => character.includes(`/people/${wedgeId}/`))
-    ).length;
-
-    console.log(count);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
