9f2ae2c00f710027bb94ebe3a9ff8a3b790c0de3
ITSNE01 <rabie.haddar1@gmail.com>
2025-02-11 03:34:41 -0800

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  for (const character of JSON.parse(body).characters) {
+    request(character, (error, response, body) => {
+      if (error) console.log(error);
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
